3358e656b3a732d1ae57b3858b134b6571d2aa6f
dcuddhy <dcuddhy@gmail.com>
2014-11-07 12:13:07 -0700

    test up.  bug fixed. awesome

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,19 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'When a user registers, they should be logged in automatically' do
+
+    visit new_registration_path
+    expect(page).to have_content("Confirm")
+    fill_in "Name", with: "Ron Swanson"
+    fill_in "Email", with: "ron@swanson.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+    click_on "Register!"
+    expect(page).to have_content("logged in successfully")
+
+  end
+
+
+
 end
